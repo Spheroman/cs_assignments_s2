8e6f9c1522bcd8e23ae79eddbc7d18acbb7a1422
Spheroman <jackwen04@gmail.com>
2023-04-08 14:36:04 +0800

    w4q3

--- current.cpp
+++ current.cpp
@@ -1,203 +1,174 @@
+#include <cstdio>
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-class fraction{
-    public:
-    int numerator;
-    int denominator;
-    fraction(int n, int d){
-        int g = gcd(n, d);
-        numerator = n/g;
-        denominator = d/g;
-    }
-    fraction(int n){
-        numerator = n;
-        denominator = 1;
-    }
-    fraction(){
-        numerator = 0;
-        denominator = 1;
-    }
-    fraction add(fraction f){
-        int n = numerator * f.denominator + f.numerator * denominator;
-        int d = denominator * f.denominator;
-        int g = gcd(n, d);
-        numerator = n/g;
-        denominator = d/g;
-        return *this;
-    }
-    fraction sub(fraction f){
-        int n = numerator * f.denominator - f.numerator * denominator;
-        int d = denominator * f.denominator;
-        int g = gcd(n, d);
-        numerator = n/g;
-        denominator = d/g;
-        return *this;
-    }
-    fraction mul(fraction f){
-        int n = numerator * f.numerator;
-        int d = denominator * f.denominator;
-        int g = gcd(n, d);
-        numerator = n/g;
-        denominator = d/g;
-        return *this;
-    }
-    fraction div(fraction f){
-        int n = numerator * f.denominator;
-        int d = denominator * f.numerator;
-        int g = gcd(n, d);
-        numerator = n/g;
-        denominator = d/g;
-        return *this;
-    }
-    bool equal(fraction f){
-        return numerator * f.denominator == f.numerator * denominator;
-    }
-    int gcd(int a, int b){
-        if (b == 0){
-            return a;
-        }
-        else{
-            return gcd(b, a % b);
-        }
-    }
-    void print(){
-        int mod = 1;
-        if(denominator<0){
-            mod = -1;
-        }
-        if (denominator == 1){
-            cout << numerator;
-        }
-        else{
-            cout << numerator*mod << "/" << denominator*mod;
-        }
-    }
-};
+class Gate
+{
+public :
+    Gate *input[2] ;
+    virtual bool output() = 0 ;
+    void setValue(Gate *, int) ;
+    void setValue(bool, int) ;
+} ;
 
-class matrix{
-public:
-    fraction** a;
-    int xl, yl;
-    matrix(int x, int y){
-        xl = x;
-        yl = y;
-        a = new fraction*[x];
-        for(int i = 0; i < x; ++i) {
-            a[i] = new fraction[y];
-        }
-    }
-    void set(int x, int y, fraction f){
-        a[x][y] = f;
-    }
-    void set(int x, int y, int n, int d){
-        a[x][y] = fraction(n, d);
-    }
-    void set(int x, int y, int n){
-        a[x][y] = fraction(n, 1);
-    }
-    matrix div(fraction f){
-        for (int i = 0; i < xl; i++){
-            for (int j = 0; j < yl; j++){
-                a[i][j].div(f);
-            }
-        }
-        return *this;
-    }
-    void print(){
-        for (int i = 0; i < xl; i++){
-            for (int j = 0; j < yl; j++){
-                a[i][j].print();
-                cout << " ";
-            }
-            cout << endl;
-        }
-    }
-    fraction determinant(){
-        if (xl != yl){
-            cout << "Not a square matrix";
-            return {0};
-        }
-        if (xl == 1){
-            return a[0][0];
-        }
-        if (xl == 2){
-            return a[0][0].mul(a[1][1]).sub(a[0][1].mul(a[1][0]));
-        }
-        fraction ret(0);
-        for (int i = 0; i < xl; i++){
-            matrix b(xl - 1, yl - 1);
-            for (int j = 0; j < xl - 1; j++){
-                for (int k = 0; k < yl - 1; k++){
-                    b.set(j, k, a[j + (j >= i)][k + (k >= 0)]);
-                }
-            }
-            fraction c = b.determinant();
-            if (i % 2 == 0){
-                ret.add(c.mul(a[i][0]));
-            }
-            else{
-                ret.sub(c.mul(a[i][0]));
-            }
-        }
-        return ret;
-    }
-    matrix transpose(){
-        matrix ret(yl, xl);
-        for (int i = 0; i < xl; i++){
-            for (int j = 0; j < yl; j++){
-                ret.set(j, i, a[i][j]);
-            }
-        }
-        return ret;
-    }
+class TRUE : public Gate
+{
+public :
+    virtual bool output() { return 1 ; }
+    void setValue(Gate *, int) {}
+    void setValue(bool, int) {}
+} ;
 
-    matrix inverse(){
-        if (xl != yl){
-            cout << "Cannot produce an inverse matrix.";
-            exit(0);
-        }
-        matrix ret = this->transpose();
-        fraction det = ret.determinant();
-        if (det.equal({0})){
-            cout << "Cannot produce an inverse matrix.";
-            exit(0);
-        }
-        for(int i = 0; i < xl; i++){
-            for(int j = 0; j < yl; j++){
-                matrix b(xl - 1, yl - 1);
-                for (int k = 0; k < xl - 1; k++){
-                    for (int l = 0; l < yl - 1; l++){
-                        b.set(k, l, a[k + (k >= i)][l + (l >= j)]);
-                    }
-                }
-                ret.set(i, j, b.determinant());
-                if ((i + j) % 2 == 1){
-                    ret.a[i][j].numerator *= -1;
-                }
-            }
-        }
-        return ret.div(det).transpose();
-    }
-};
+class FALSE : public Gate
+{
+public :
+    virtual bool output() { return 0 ; }
+    void setValue(Gate *, int) {}
+    void setValue(bool, int) {}
+} ;
 
-int main(){
-    int n, m;
-    cin >> n >> m;
-    cin.ignore();
-    matrix a(n, m);
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++){
-            int x;
-            cin >> x;
-            cin.ignore();
-            a.set(i, j, x);
-        }
+TRUE t ;
+FALSE f ;
+
+void Gate::setValue(bool val, int pin)
+{
+    if(val) this -> input[pin] = &t ;
+    else this -> input[pin] = &f ;
+}
+
+void Gate::setValue(Gate *gate, int pin) { this -> input[pin] = gate ; }
+
+class NOT : public Gate
+{
+public :
+    virtual bool output() { return !(this -> input[0] -> output()) ; }
+    void setValue(bool val, int pin = 0)
+    {
+        if(val) this -> input[0] = &t ;
+        else this -> input[0] = &f ;
     }
-    a.transpose().print();
-    cout << endl;
-    a.inverse().print();
-    return 0;
+    void setValue(Gate* gate, int pin = 0) { this -> input[0] = gate ; }
+} ;
+
+class NAND : public Gate
+{
+public :
+    virtual bool output() { return !(this -> input[0] -> output()) || !(this -> input[1] -> output()) ; }
+} ;
+
+class NOR : public Gate
+{
+public :
+    virtual bool output() { return !(this -> input[0] -> output()) && !(this -> input[1] -> output()) ; }
+} ;
+
+class AND : public Gate
+{
+public :
+    AND() : Gate() {}
+    virtual bool output() { return this -> input[0] -> output() && this -> input[1] -> output() ;}
+private :
+    Gate *component[2] ;
+} ;
+
+class OR : public Gate
+{
+public :
+    OR() : Gate() {}
+    virtual bool output() { return this -> input[0] -> output() || this -> input[1] -> output() ;}
+private :
+    Gate *component[2] ;
+} ;
+
+class XOR : public Gate
+{
+public :
+    XOR() : Gate() {}
+    virtual bool output() { return this -> input[0] -> output() ^   this -> input[1] -> output() ;}
+private :
+    Gate *component[2] ;
+} ;
+
+class Adder
+{
+public :
+    virtual void setValue(bool, int) = 0 ;
+    virtual void setValue(Gate *, int) = 0 ;
+    virtual Gate *sum() = 0 ;
+    virtual Gate *carryOut() = 0 ;
+} ;
+
+class OneBitHalfAdder : public Adder
+{
+public :
+    OneBitHalfAdder() {
+        component[0] = new XOR();
+        component[1] = new AND();
+}
+    virtual void setValue(bool val, int pin) {
+        component[0] -> setValue(val, pin);
+        component[1] -> setValue(val, pin);
+}
+    virtual void setValue(Gate *gate, int pin) {
+        component[0] -> setValue(gate, pin);
+        component[1] -> setValue(gate, pin);
+}
+    virtual Gate *sum() {
+        return component[0];
+}
+    virtual Gate *carryOut() {
+        return component[1];
+}
+private :
+    Gate *component[2] ;
+} ;
+
+class OneBitFullAdder : public Adder
+{
+public :
+    OneBitFullAdder() {
+        a[0] = new OneBitHalfAdder() ;
+        a[1] = new OneBitHalfAdder() ;
+        carry = new OR() ;
+}
+    virtual void setValue(bool val, int pin) {
+        if (pin == 2) a[1] -> setValue(val, 0) ;
+        else a[0] -> setValue(val, pin) ;
+}
+    virtual void setValue(Gate *gate, int pin) {
+        if (pin == 2) a[1] -> setValue(gate, 1) ;
+        else a[0] -> setValue(gate, pin) ;
+}
+    virtual Gate *sum() {
+        a[1] -> setValue(a[0] -> sum(), 0) ;
+        return a[1] -> sum() ;
+}
+    virtual Gate *carryOut() {
+        carry -> setValue(a[0] -> carryOut(), 0) ;
+        a[1] -> setValue(a[0] -> sum(), 1) ;
+        carry -> setValue(a[1] -> carryOut(), 1) ;
+        return carry;
+}
+private :
+
+    Adder *a[2] ;
+    Gate *carry = &f;
+} ;
+
+int main()
+{
+    OneBitFullAdder a ;
+    a.setValue(&t, 0) ;
+    a.setValue(&f, 1) ;
+    a.setValue(&f, 2);
+    cout << a.sum() -> output() ;
+    cout << a.carryOut() -> output() ;
+    OneBitFullAdder b ;
+    b.setValue(&t, 0) ;
+    b.setValue(a.sum(), 1) ;
+    b.setValue(a.carryOut(), 2);
+    cout << b.sum() -> output();
+    cout << b.carryOut() -> output();
+    return 0 ;
 }
